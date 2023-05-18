ccf6eeb0527f90148bfaa379f0868159a056a6e3
kirankiran88 <kirankiran88>
2023-05-18 16:30:45 +0530

    credit

--- transaction/src/main/java/com/evolvus/transaction/AsbaEntity.java
+++ transaction/src/main/java/com/evolvus/transaction/AsbaEntity.java
@@ -5,6 +5,23 @@ public class AsbaEntity {
 	
 	private String name;
 	
+	private int id;
 	
+	public String getname() {
+		return name ;
+	}
+	
+	public void setname(String name) {
+		this.name=name;
+	}
+	
+	public int getid() {
+		return id ;
+		
+	}
+	
+	public void setid(int id) {
+		this.id=id;
+	}
 
 }
---
+++ transaction/src/main/java/com/evolvus/transaction/credit.java
@@ -0,0 +1,11 @@
+package com.evolvus.transaction;
+
+public class credit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("your salary has been credited...");
+
+	}
+
+}
